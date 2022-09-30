b5243157240846740e206f4fd4498359fdc23f54
devarturmartins <arturvl1@hotmail.com>
2022-09-30 03:57:29 -0300

    segundo requisito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species.find((animais) => animais.name === animal)
+    .residents.every((idades) => idades.age >= age);  // seu código aqui
 }
+
+console.log(getAnimalsOlderThan('lions', 5));
 
 module.exports = getAnimalsOlderThan;
