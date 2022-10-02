700d3d47c0d6779bebb2fae747e885de57837b7b
devarturmartins <arturvl1@hotmail.com>
2022-10-02 19:33:50 -0300

    r.6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,28 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Retorna undefined quando não recebe parametro', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+  it('Verifica se o retorno é um objeto', () => {
+    expect(typeof handlerElephants('artur')).toBe('object');
+  });
+  it('Retorna a quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Retorna um array com a relacao de nomes de todos os elefantes', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+  it('Retorna a média de idade dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+  it('Retorna a localização dos elefantes dentro do Zoológico', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('Retorna a popularidade dos elefantes', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+  it('Retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+});
