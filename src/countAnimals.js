93d74191ee41fc891df82389de9da0e56bc03b60
devarturmartins <arturvl1@hotmail.com>
2022-10-02 17:57:37 -0300

    r.5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const objVazio = {};
+  if (animal === undefined) {
+    species.forEach((elemento) => {
+      objVazio[elemento.name] = elemento.residents.length;
+    });
+    return objVazio;
+  }
+  if (Object.keys(animal).length === 1) {
+    return species.find((elemento) => elemento.name === Object.values(animal)[0]).residents.length;
+  }
+  if (Object.keys(animal).length === 2) {
+    return species.find((e) => e.name === Object.values(animal)[0])
+      .residents.filter((element) => element.sex === Object.values(animal)[1]).length;
+  }
 }
-
+console.log(countAnimals({ specie: 'lions', sex: 'female' }));
 module.exports = countAnimals;
