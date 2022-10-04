7453aa05cc6a3a082622217a528ab76b8a729860
devarturmartins <arturvl1@hotmail.com>
2022-10-03 21:56:37 -0300

    r. 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const childs = entrants.filter((element) => element.age < 18).length;
+  const adults = entrants.filter((pessoas) => pessoas.age >= 18 && pessoas.age < 50).length;
+  const seniors = entrants.filter((idosos) => idosos.age >= 50).length;
+  const newObject = {
+    child: childs,
+    adult: adults,
+    senior: seniors,
+  };
+  return newObject;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const client = countEntrants(entrants);
+  let soma = 0;
+  soma = client.child * prices.child + client.adult * prices.adult + client.senior * prices.senior;
+  return soma;
 }
-
 module.exports = { calculateEntry, countEntrants };
