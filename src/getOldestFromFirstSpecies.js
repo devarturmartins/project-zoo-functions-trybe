ca6b40f3948001466624a05591c109b2fcab4165
devarturmartins <arturvl1@hotmail.com>
2022-10-04 21:32:21 -0300

    r. 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animais = employees.find((e) => e.id === id).responsibleFor[0];
+  const idadeMaisVelho = species.find((element) => element.id === animais)
+    .residents.reduce((acc, curr) => {
+      if (curr.age > acc) {
+        return curr.age;
+      }
+      return acc;
+    }, 0);
+  const animalVelho = species.find((e) => e.id === animais)
+    .residents.find((element) => element.age === idadeMaisVelho);
+  return Object.values(animalVelho);
 }
-
+console.log(getOldestFromFirstSpecies('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
 module.exports = getOldestFromFirstSpecies;
