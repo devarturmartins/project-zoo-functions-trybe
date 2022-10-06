c9bdae9870bddb0c4c7b83f718127e855fc3d64c
devarturmartins <arturvl1@hotmail.com>
2022-10-06 01:14:32 -0300

    r. 12 e 13

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -22,4 +22,16 @@ describe('Testes da função getOpeningHours', () => {
   it('Para os argumentos Wednesday e 09:00-PM deve retornar a string The zoo is closed', () => {
     expect(getOpeningHours('Wednesday', '09:00-PM')).toEqual('The zoo is closed');
   });
+  it('Dias como nomes incompletos', () => {
+    expect(getOpeningHours('Thu', '09:00-AM')).toThrowError('The day must be valid. Example: Monday');
+  });
+  it('Para os argumentos Friday e 09:00-ZM deve lançar uma exceção com a mensagem: The abbreviation must be AM or PM', () => {
+    expect(getOpeningHours('Friday', '09:00-ZM')).toThrowError('The abbreviation must be \'AM\' or \'PM\'');
+  });
+  it('Para os argumentos Saturday e C9:00-AM deve lançar uma exceção com a mensagem: The hour should represent a number', () => {
+    expect(getOpeningHours('Saturday', 'C9:00-AM')).toThrowError('The hour should represent a number');
+  });
+  it('Para argumentos Sunday e 09:c0-AM', () => {
+    expect(getOpeningHours('Sunday', '09:c0-AM')).toThrowError('The minutes should represent a number');
+  });
 });
